f4c5fb7e6260903f904b92d19f31047816be0aab
radu-matei <matei.radu94@gmail.com>
2017-11-25 21:37:20 +0200

    Add simple SignalR chat
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ simple-chat/ChatHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalr_aspnetcore
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.InvokeAsync("broadcastMessage", name, message);
+        }
+    }
+}
