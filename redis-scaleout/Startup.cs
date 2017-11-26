000f2546cddabffb0a7d0486139b90c8d6603a87
radu-matei <matei.radu94@gmail.com>
2017-11-26 18:26:56 +0200

    Add Redis scaleout sample
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ redis-scaleout/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace redis_scaleout
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR()
+                .AddRedis(options => options.Factory = writer =>
+                {
+                    return ConnectionMultiplexer.Connect("localhost", writer);
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("chat");
+            });
+        }
+    }
+}
