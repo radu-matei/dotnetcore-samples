407cc2a61f5f402d109b569f43734bf27c8441fc
radu-matei <matei.radu94@gmail.com>
2017-11-26 16:22:49 +0200

    Add example that uses the binary protocol
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ binary/console-client-binary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace console_client_binary
+{
+    class Program
+    {
+        private static HubConnection _connection;
+        static void Main(string[] args)
+        {
+
+            StartConnectionAsync();
+            _connection.On<string, string>("broadcastMessage", (name, message) =>
+            {
+                Console.WriteLine($"{name} said: {message}");
+            });
+
+            Console.ReadLine();
+            DisposeAsync();
+        }
+
+
+        public static async Task StartConnectionAsync()
+        {
+            _connection = new HubConnectionBuilder()
+                 .WithUrl("http://localhost:5000/chat")
+                 .WithConsoleLogger()
+                 .WithMessagePackProtocol()
+                 .Build();
+
+            await _connection.StartAsync();
+        }
+
+        public static async Task DisposeAsync()
+        {
+            await _connection.DisposeAsync();
+        }
+    }
+}
